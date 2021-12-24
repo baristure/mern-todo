7f7f87ded13c2c74fcebabfda7112c0099566361
Barış TÜRE <baristure@hotmail.com>
2021-12-25 00:09:22 +0300

    Update api url

--- frontend/src/api/apiconfig.js
+++ frontend/src/api/apiconfig.js
@@ -1,2 +1,2 @@
-// export const apiBaseUrl = "http://46.101.110.125:5000";
-export const apiBaseUrl = "http://localhost:5000";
+export const apiBaseUrl = "http://46.101.110.125:5000";
+//export const apiBaseUrl = "http://localhost:5000";
